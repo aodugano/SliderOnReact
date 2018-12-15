47528e0a3afad29bcf0793a0f06e43a5d647a5ad
aodugano <44617790+aodugano@users.noreply.github.com>
2018-12-15 20:34:01 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.png';
+
+const HeaderWrapper = styled.header`
+    width: 100%;
+    height: 49px;
+    background-color: #242424;
+    opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+    height: 89px;
+    padding-top: 20px;
+
+`
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    -webkit-background-size: cover;
+    background-size: cover;
+
+`
 class App extends Component {
   render() {
     return ( 
-      <article className="default">
-        <FullName name="Alex" surname="Duganov" link="google.com" />
-        <FullName name="Richard" surname="Nikson" link="ya.ru" />
-        <FullName name="Sofia" surname="Weider" link="star.war" />
-      </article>
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+         </HeaderWrapper> 
+         <MenuWrapper>
+            <Grid>
+
+              <Menu />
+            </Grid>
+          </MenuWrapper>
+          <MainWrapper>
+              <Grid>
+                <Main />
+              </Grid>
+          </MainWrapper>
+      </div>
     );
   }
 }
 
-
-
-function FullName(props) {
-    return (
-      <article>
-        <h2> Name {props.name}, surname - {props.surname}</h2>
-        <a href={props.link}> Link to my profile </a>
-      </article>
-    )
-}
 
 
 export default App;
